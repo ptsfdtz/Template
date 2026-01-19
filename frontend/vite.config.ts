501977aa921faf7684d8a0757ead034e157d24ae
ptsfdtz <pitousanfadetuzi@gmail.com>
2026-01-19 11:03:35 +0800

    feat: configure Prettier and update TypeScript settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,16 @@
-import {defineConfig} from 'vite'
-import {fileURLToPath, URL} from 'node:url'
-import react from '@vitejs/plugin-react'
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+});
